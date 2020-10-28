78dc7590bd65b780e4465e2f4f860c3025059413
Kamil Salawa <emir.salawa@gmail.com>
2020-10-28 13:37:25 +0000

    finished

--- message.mjs
+++ message.mjs
@@ -1,16 +1,14 @@
- import MessageMixer from './messageMixer.mjs';
- 
- function displayMessage() {
-    console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-    console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-    console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-    console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(MessageMixer.encode("What is the color of the sky?"));
-    console.log(MessageMixer.palindrome("What is the color of the sky?"));
-    console.log(MessageMixer.pigLatin("What is the color of the sky?", "🐷"));
-  }
-  
-  displayMessage();
+import { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin} from './messageMixer.mjs';
 
-  
+function displayMessage() {
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome(`how's you babes?`));
+  console.log(pigLatin('holla so you are my', ' '));
+}
+
+displayMessage();
--- messageMixer.mjs
+++ messageMixer.mjs
@@ -1,6 +1,4 @@
-const MessageMixer = {};
-
-MessageMixer.countCharacter = function (inputString, inputCharacter) {
+function countCharacter(inputString, inputCharacter) {
   let count = 0;
   let string = inputString.toLowerCase();
   let character = inputCharacter.toLowerCase();
@@ -9,62 +7,55 @@ MessageMixer.countCharacter = function (inputString, inputCharacter) {
          count++;
       }
     }
-  return count; 
+  return count;
 };
 
-
-
-MessageMixer.capitalizeFirstCharacterOfWords = function (string) {
-  let arr = string.split(" ");  
-    for (let i = 0; i < arr.length; i++) {  
+function capitalizeFirstCharacterOfWords(string) {
+  let arr = string.split(" ");
+    for (let i = 0; i < arr.length; i++) {
       let word = arr[i];
-        arr[i] = word[0].toUpperCase() + word.substring(1); 
+        arr[i] = word[0].toUpperCase() + word.substring(1);
     }
-  return arr.join(" "); 
+  return arr.join(" ");
 };
 
 
-
-MessageMixer.reverseWord = function (word) {
+function reverseWord(word) {
   return word.split("").reverse().join("");
 };
 
-
-MessageMixer.reverseAllWords = function (sentence) {
+function reverseAllWords(sentence) {
   let words = sentence.split(" ");
     for (let i = 0; i < words.length; i++) {
-      words[i] = MessageMixer.reverseWord(words[i]);
+      words[i] = reverseWord(words[i]);
     }
    return words.join(" ");
 };
 
 
-
-MessageMixer.replaceFirstOccurence = function (string, toBeReplaced, replaceWith) {
+function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
   return string.replace(toBeReplaced, replaceWith);
 };
 
 
-MessageMixer.replaceAllOccurrences = function (string, toBeReplaced, replaceWith) {
+function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
   return string.split(toBeReplaced).join(replaceWith);
 };
 
-
-MessageMixer.encode = function (string) {
+function encode(string) {
   let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
     for (let key in replacementObject) {
-      string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]); 
-    }	
+      string = replaceAllOccurrences(string, key, replacementObject[key]);
+    }
     return string;
 };
 
-MessageMixer.palindrome = function (str){
-    return `${str} ${MessageMixer.reverseWord(str)}`;
+function palindrome(word){
+  return word + " " + reverseWord(word);
 };
 
-MessageMixer.pigLatin = function (sentence, character) {
-    return sentence.split(' ').join(character + ' ');
-
+function pigLatin(sentence, character) {
+  return sentence.split(" ").join(character + "🐷 ");
 };
 
-export default MessageMixer;
+export { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin };
